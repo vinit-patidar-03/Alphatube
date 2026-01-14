4daee26900c1e421194f2474539ea08cafc9e9e0
Vinit Patidar <Vinitpatidar780@gmail.com>
2026-01-14 13:28:40 +0530

    update: changed env var

--- src/API/YoutubeAPI.js
+++ src/API/YoutubeAPI.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const BaseUrl = 'https://yt-api.p.rapidapi.com'
-const apikey1 = process.env.REACT_APP_APIKEY1;
+const apikey = process.env.REACT_APP_APIKEY;
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': apikey1,
+    'X-RapidAPI-Key': apikey,
     'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
   }
 };
